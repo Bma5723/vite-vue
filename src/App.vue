e5021d541e457aae57a91ec342b099ad852beddf
Bader Alkatheeri <baderalkatheeri@Baders-MacBook-Pro.local>
2023-12-06 20:38:57 -0500

    card added

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+<header>
+<div class="buttons">
+  <button class="duplicate">Duplicate last card</button>
+  <button class="modifytitle">Change Name</button>
+  <button id="deletelastcard">Delete</button>
+<div class="btn-wrapper">
+  <button href="https://gopsusports.com/sports/football/roster/nicholas--singleton/14065">Nicholas Singleton</button>
+</div>
+<div class="wrapper">
+  <div class="container">
+  <img class="image" src="https://www.inquirer.com/resizer/JVse2HXoMM-7NcrCYLy8QPlLBnY=/800x533/smart/filters:format(webp)/cloudfront-us-east-1.images.arcpublishing.com/pmn/GPCW7HKIGBB35GT5JNSHOYPF7Y.jpeg"/>
+  <div class="header">
+    <h3>Nicholas Singleton</h3>
+    <h4>PSU Football player</h4>
   </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+  <details class="details">
+    <summary>Career Stats</summary>
+    <div>
+      <ul>
+        <li>Big Ten's Freshman of the Year in 2022..</li>
+        <li>Penn State running back</li>
+       </ul>
+    </div>
+    </details>
+</div>
